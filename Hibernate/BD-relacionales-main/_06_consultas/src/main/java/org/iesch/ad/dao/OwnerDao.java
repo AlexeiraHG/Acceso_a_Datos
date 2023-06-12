a052c750b9d712c001843521777bb71e3877e720
Alexeira <gherediag@iesch.org>
2023-06-12 23:02:30 +0200

    practicas

--- Hibernate/BD-relacionales-main/_06_consultas/src/main/java/org/iesch/ad/dao/OwnerDao.java
+++ Hibernate/BD-relacionales-main/_06_consultas/src/main/java/org/iesch/ad/dao/OwnerDao.java
@@ -6,7 +6,6 @@ import org.iesch.ad.modelo.Pet;
 
 import java.util.List;
 
-
 public class OwnerDao {
 
     EntityManagerFactory entityManagerFactory;
@@ -19,50 +18,47 @@ public class OwnerDao {
         transaction = entityManager.getTransaction();
     }
 
-    //deberia estar en PETDAO!!!! Perdon!
-    public List<Pet> buscaTodasLasMascotas(int id){
+    // deberia estar en PETDAO!!!! Perdon!
+    public List<Pet> buscaTodasLasMascotas(int id) {
         Query queryPets = entityManager.createQuery("select e from Pet e where id = :id").setParameter("id", id);
         return queryPets.getResultList();
 
     }
 
-    public List<Owner> buscaPorCorreo(String dominio){
-        TypedQuery<Owner> namedQueryCorreo = entityManager.createNamedQuery("dueno.BuscaCorreos", Owner.class );
-        namedQueryCorreo.setParameter("correo","%"+dominio);
+    public List<Owner> buscaPorCorreo(String dominio) {
+        TypedQuery<Owner> namedQueryCorreo = entityManager.createNamedQuery("dueno.BuscaCorreos", Owner.class);
+        namedQueryCorreo.setParameter("correo", "%" + dominio);
         return namedQueryCorreo.getResultList();
     }
 
     public List<Owner> buscaApellido(String apellido) {
-        Query queryPets = entityManager.createQuery("select e from Owner e where e.surname = :apellido").setParameter("apellido", apellido);
+        Query queryPets = entityManager.createQuery("select e from Owner e where e.surname = :apellido")
+                .setParameter("apellido", apellido);
         return queryPets.getResultList();
     }
 
     public List<Owner> buscaMascotasChipPeso150() {
-        //sql = "select o.* from pet p join owner o  on o.id = p.owner_id where haschip  = true and weight > 150;"
-        //Query queryPets = entityManager.createQuery("select e.owner from Pet e where e.hasChip = true and e.weight > 150");
-        Query queryPets = entityManager.createQuery("select o from Pet as p join Owner as o  with o.id = p.owner.id where p.hasChip  = true and p.weight > 150");
+        // sql = "select o.* from pet p join owner o on o.id = p.owner_id where haschip
+        // = true and weight > 150;"
+        // Query queryPets = entityManager.createQuery("select e.owner from Pet e where
+        // e.hasChip = true and e.weight > 150");
+        Query queryPets = entityManager.createQuery(
+                "select o from Pet as p join Owner as o  with o.id = p.owner.id where p.hasChip  = true and p.weight > 150");
         return queryPets.getResultList();
     }
 
-    public void insertaPrevioCheck (Owner o){
-        Query consulta = entityManager.createQuery("select count(o) from Owner o where name = :nombre and surname  = :apellido").setParameter("nombre", o.getName()).setParameter("apellido", o.getSurname());
-        if(consulta.getResultList().isEmpty()){
+    public void insertaPrevioCheck(Owner o) {
+        Query consulta = entityManager
+                .createQuery("select count(o) from Owner o where name = :nombre and surname  = :apellido")
+                .setParameter("nombre", o.getName()).setParameter("apellido", o.getSurname());
+        if (consulta.getResultList().isEmpty()) {
 
             transaction.begin();
-        entityManager.persist(o);
-        transaction.commit();
-        }else System.out.println("YA existía");
-
-
-    }
-
-    public void insertaMascotaAndId(Pet p, int id){
-        Owner o = entityManager.find(Owner.class, id);
-        o.
-
-
+            entityManager.persist(o);
+            transaction.commit();
+        } else
+            System.out.println("YA existía");
 
     }
-
 
 }
